55e27a58398e4c753d7a9f3125b5f2afaa2321b5
Lukasz Ruszczyk <lukasz.ruszczyk@gmail.com>
2019-06-13 20:51:07 +0200

    feat: week 4

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const age = 33;
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lata.☠️`);
+
+const box = document.querySelector('.box');
+console.log(box.innerHTML);
+
+const boxjs = document.querySelector('.box__js');
+boxjs.innerHTML = `Nazywam się ${name} i mam ${age} lata.☠️`;
