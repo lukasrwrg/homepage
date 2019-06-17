6171cc4e2930bfc1ec28282f8913216c6bcb3785
Lukasz Ruszczyk <lukasz.ruszczyk@gmail.com>
2019-06-18 00:46:49 +0200

    add BEM name

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lata.☠️`);
 
-const box = document.querySelector('.box');
+const box = document.querySelector('.about__box');
 console.log(box.innerHTML);
 
-const boxjs = document.querySelector('.box__js');
+const boxjs = document.querySelector('.about__box--js');
 boxjs.innerHTML = `Nazywam się ${name} i mam ${age} lata.☠️`;
